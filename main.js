524fa9515c41ba2953751fb159f3c7e1da59cc64
reaper <ahoy@barelyhuman.dev>
2023-03-24 08:26:39 +0530

    fp version

--- main.js
+++ main.js
@@ -1,148 +1,227 @@
-import { animate, linear } from "popmotion";
-import randomWords from "random-words";
-import { effect, reactive } from "./rndr.js";
+import { animate, linear } from 'popmotion'
+import randomWords from 'random-words'
+import {
+  compose,
+  curry,
+  fromEvent,
+  fromSubscription,
+  IO,
+  map,
+  prop,
+  run,
+  unary,
+} from './fp.js'
 
-const queue = Promise.prototype.then.bind(Promise.resolve());
-const ignoreKeys = ["ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp"];
-const input = document.querySelector("#typer-input");
-const preview = document.querySelector("#typer-preview");
-const caret = document.querySelector("#caret");
-const speed = document.querySelector("#speed");
+// Steams and subscriptions to reset when state resets
+let charStream, keydownUnsub, keyupUnsub
 
-let autoTypeListner;
-let startTime;
+const ignoreKeys = ['ArrowLeft', 'ArrowRight', 'ArrowDown', 'ArrowUp']
+const wordsToChars = words => words.join(' ').split('').concat(' ')
+const generateRandomWords = () => wordsToChars(randomWords(30))
+const charKey = (char, id) => `#${char.trim() || 'space'}-${id}`
 
-const state = reactive({ words: [], input: "", speed: 0 });
+const $ = sel => IO.of(() => document.querySelector(sel))
 
-input.addEventListener("keydown", (e) => {
-  if (e.code === "Escape") {
-    e.preventDefault();
-    resetState();
+const resetInput = input => {
+  input.value = ''
+  return input
+}
+
+const resetPreview = preview => {
+  preview.innerHTML = ''
+  return preview
+}
+
+const focusInput = input => {
+  input.focus()
+  return input
+}
+
+const setMaxLength = curry((maxLength, input) => {
+  input.maxLength = maxLength
+  return input
+})
+
+const pickEvCode = compose(e => [e.code, e])
+
+const isEscape = ([code, e]) => {
+  return [code === 'Escape', e]
+}
+
+const isIgnoredKey =
+  keysToIgnore =>
+  ([code, e]) =>
+    [keysToIgnore.indexOf(code) > -1, e]
+
+const resetIfEscape = compose(pickEvCode, isEscape, ([stop, e]) => {
+  if (stop) {
+    e.preventDefault()
+    e.stopPropagation()
+    charStream.next(generateRandomWords())
   }
-  if (ignoreKeys.indexOf(e.code) > -1) {
-    console.log("key to ignore");
-    e.preventDefault();
-    return;
-  }
-});
+  return e
+})
 
-input.addEventListener(
-  "keyup",
-  (e) => {
-    state.input = e.target.value;
-  },
-  false
-);
-
-function resetState() {
-  state.input = "";
-  startTime = null;
-  input.value = "";
-  preview.innerHTML = "";
-  setupWords(randomWords(30));
-}
-
-function setupWords(words) {
-  state.words = words.join(" ").split("").concat(" ");
-}
-
-function renderSpeed() {
-  effect(() => {
-    speed.innerHTML = state.speed + " WPM";
-  });
-}
-
-function renderWords() {
-  effect(() => {
-    if (state.input.length == 0) {
-      if (!startTime) {
-        startTime = Date.now();
-      }
+const stopIfIgnoredKey = compose(
+  pickEvCode,
+  isIgnoredKey(ignoreKeys),
+  ([stop, e]) => {
+    if (stop) {
+      e.preventDefault()
+      e.stopPropagation()
     }
+    return e
+  }
+)
 
-    const elms = state.words.map((x, index) => {
-      let exists = true;
-      let elm = document.getElementById(`${x}-${index}`);
-      if (!elm) {
-        exists = false;
-        elm = document.createElement("span");
-        elm.classList.add("typer-letter");
-        elm.id = `${x}-${index}`;
+const isCharlistValid = curry((charList, val) =>
+  map((char, index) => {
+    if (!val[index]) {
+      return { char, valid: null, index }
+    }
+    if (char === val[index]) {
+      return { char, valid: true, index }
+    } else {
+      return { char, valid: false, index }
+    }
+  }, charList)
+)
+
+const onKeydownOnInput = compose(resetIfEscape, stopIfIgnoredKey)
+
+// TODO: Impure, can be ignored if `elm` is considered a dependency adaptor
+const updateSpanElement = charState => elm => {
+  elm.classList.remove('valid')
+  elm.classList.remove('invalid')
+
+  if (charState.valid === null) {
+    return [false, elm]
+  }
+
+  if (charState.valid) {
+    elm.classList.add('valid')
+  } else {
+    elm.classList.add('invalid')
+  }
+  return [true, elm]
+}
+
+// TODO: Impure
+const updateSpanStateAndCaret = curry((charSet, node) =>
+  compose(
+    map(updateSpanElement(charSet)),
+    map(([move, e]) => {
+      if (move) {
+        moveCaretTo(e)
       }
-      elm.textContent = x;
+    })
+  )(node)
+)
 
-      if (!exists) {
-        preview.append(elm);
-      }
+const updateSpanByValue = curry((charList, val) => {
+  // TODO: Impure
+  map(
+    charSet =>
+      run(
+        updateSpanStateAndCaret(
+          charSet,
+          $(charKey(charSet.char, charSet.index))
+        )
+      ),
+    isCharlistValid(charList, val)
+  )
 
-      elm.classList.remove("valid");
-      elm.classList.remove("invalid");
+  return val
+})
 
-      if (state.input[index]) {
-        if (state.words[index] === state.input[index]) {
-          elm.classList.add("valid");
-          state.speed = calcSpeed(startTime, state.input.length);
-        } else {
-          elm.classList.add("invalid");
-        }
-      }
+const respositionCaret = curry((toNodeIO, val) => {
+  // TODO: Impure
+  !val.length && run(map(e => moveCaretTo(e, true), toNodeIO))
+  return val
+})
 
-      return { elm };
-    });
+const onKeyupOnInput = charList =>
+  compose(
+    prop('target'),
+    prop('value'),
+    updateSpanByValue(charList),
+    respositionCaret($(charKey(charList[0], 0)))
+  )
 
-    const currentElm = elms[state.input.length ? state.input.length : 0];
+const charsToPreview = charList => elm =>
+  charList.map((char, index) =>
+    IO.of(() => {
+      const span = document.createElement('span')
+      span.id = `${char.trim() || 'space'}-${index}`
+      span.classList.add('typer-letter')
+      span.innerText = char
+      elm.appendChild(span)
+      return span
+    })
+  )
 
-    if (!currentElm) return;
-
-    const box = currentElm.elm.getBoundingClientRect();
-    const width = 3;
-    const caretHeight = box.height / 1.4;
+function moveCaretTo(node, before) {
+  const effect = map(caret => {
+    const box = node.getBoundingClientRect()
+    const width = 3
+    const caretHeight = box.height / 1.4
     Object.assign(caret.style, {
-      top: box.y + (box.height - caretHeight * 1.22) + "px",
-      height: caretHeight + "px",
-      width: width + "px",
-      bottom: box.height + caretHeight + "px",
-    });
+      top: box.y + (box.height - caretHeight * 1.22) + 'px',
+      height: caretHeight + 'px',
+      width: width + 'px',
+      bottom: box.height + caretHeight + 'px',
+    })
+    let target = box.x - width
+    if (!before) {
+      target += box.width
+    }
     animate({
-      type: "keyframes",
+      type: 'keyframes',
       ease: linear,
       duration: 125,
       from: caret.style.left,
-      to: box.x - width + "px",
-      onUpdate: (latest) => (caret.style.left = latest),
-    });
-  });
+      to: target + 'px',
+      onUpdate: latest => (caret.style.left = latest),
+    })
+  })($('#caret'))
+
+  run(effect)
 }
 
-function autoType() {
-  effect(() => {
-    let counter = 0;
-    input.value = "";
-    const statement = state.words.join("");
-    if (autoTypeListner) {
-      clearInterval(autoTypeListner);
-    }
-    autoTypeListner = setInterval(() => {
-      const char = statement[counter++];
-      const evt = new KeyboardEvent("keyup", { key: char });
-      input.value += char;
-      input.dispatchEvent(evt);
-    }, 30);
-  });
-}
+// Effect Executions
+charStream = fromSubscription({
+  next: charList => {
+    keydownUnsub && keydownUnsub()
+    keyupUnsub && keyupUnsub()
 
-function calcSpeed(startTime, typedCharacters) {
-  const minutes = 60 * 1000;
-  const totalTimeInMills = Date.now() - startTime;
-  const totalTimeInMinutes = totalTimeInMills / minutes;
-  const typedWords = typedCharacters / 5;
-  return Math.floor(typedWords / totalTimeInMinutes);
-}
+    run(
+      map(
+        compose(resetPreview, charsToPreview(charList), map(run)),
+        $('#typer-preview')
+      )
+    )
 
-(function main() {
-  resetState();
-  renderWords();
-  renderSpeed();
-  input.focus();
-  // autoType();
-})();
+    run(
+      map(e => {
+        moveCaretTo(e, true)
+      }, $(charKey(charList[0], 0)))
+    )
+
+    run(
+      map(e => {
+        compose(resetInput, focusInput, setMaxLength(charList.length - 1))(e)
+
+        keydownUnsub = fromEvent(e, 'keydown').subscribe(
+          unary(onKeydownOnInput)
+        )
+
+        keyupUnsub = fromEvent(e, 'keyup').subscribe(
+          unary(onKeyupOnInput(charList))
+        )
+      }, $('#typer-input'))
+    )
+  },
+  complete: () => {},
+})
+
+charStream.next(generateRandomWords())

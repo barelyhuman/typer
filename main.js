af62303ac85285091cfd5ba4b0b9486cf48ed8ea
Reaper Gelera <ahoy@barelyhuman.dev>
2023-06-17 15:14:11 +0530

    chore: cleanup

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-import { animate, linear } from "popmotion";
-import randomWords from "random-words";
-import { effect, reactive } from "./rndr.js";
+import { animate, linear } from 'popmotion'
+import randomWords from 'random-words'
+import { effect, reactive } from './rndr.js'
 
-const ignoreKeys = ["ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp"];
-const allowedKeys = ["Space", "Semicolon", "Equal", "Comma", "Minus", "Period", "Slash", "Backquote", "BracketLeft", "BracketRight", "Backslash", "Quote"];
-const input = document.querySelector("#typer-input");
-const preview = document.querySelector("#typer-preview");
-const caret = document.querySelector("#caret");
-const speed = document.querySelector("#speed");
+const ignoreKeys = ['ArrowLeft', 'ArrowRight', 'ArrowDown', 'ArrowUp']
+const allowedKeys = [
+  'Space',
+  'Semicolon',
+  'Equal',
+  'Comma',
+  'Minus',
+  'Period',
+  'Slash',
+  'Backquote',
+  'BracketLeft',
+  'BracketRight',
+  'Backslash',
+  'Quote',
+]
+const input = document.querySelector('#typer-input')
+const preview = document.querySelector('#typer-preview')
+const caret = document.querySelector('#caret')
+const speed = document.querySelector('#speed')
 
-const keyboardKeyCodeMap = {  // not added all keys because out of scope as of now
-  ' ': 'Space', 
+const keyboardKeyCodeMap = {
+  // not added all keys because out of scope as of now
+  ' ': 'Space',
   '0': 'Digit0',
   '1': 'Digit1',
   '2': 'Digit2',
@@ -46,7 +60,7 @@ const keyboardKeyCodeMap = {  // not added all keys because out of scope as of n
   'w': 'KeyW',
   'x': 'KeyX',
   'y': 'KeyY',
-  'z': 'KeyZ', 
+  'z': 'KeyZ',
   ';': 'Semicolon',
   '=': 'Equal',
   ',': 'Comma',
@@ -56,185 +70,190 @@ const keyboardKeyCodeMap = {  // not added all keys because out of scope as of n
   '[': 'BracketLeft',
   '\\': 'Backslash',
   ']': 'BracketRight',
-  "'": 'Quote'
-};
+  "'": 'Quote',
+}
 
-let autoTypeListener;
-let startTime;
+let autoTypeListener
+let startTime
 
-const state = reactive({ words: [], input: "", speed: 0 });
+const state = reactive({ words: [], input: '', speed: 0 })
 
-input.addEventListener("keydown", (e) => {
-  if (e.code === "Escape") {
-    e.preventDefault();
-    resetState();
+input.addEventListener('keydown', e => {
+  if (e.code === 'Escape') {
+    e.preventDefault()
+    resetState()
   }
-  if (e.code === "Backspace") {
-    e.preventDefault();
-    handleBackspacePress();
+  if (e.code === 'Backspace') {
+    e.preventDefault()
+    handleBackspacePress()
   }
   if (ignoreKeys.indexOf(e.code) > -1) {
-    console.log("key to ignore");
-    e.preventDefault();
-    return;
+    console.log('key to ignore')
+    e.preventDefault()
+    return
   }
-});
+})
 
-input.addEventListener(
-  "keyup",
-  (e) => handleKeyUp(e),
-  false
-);
+input.addEventListener('keyup', e => handleKeyUp(e), false)
 
-function getCurrentElmAndIndex () {
-  const currentIndex = state.input.length ? state.input.length - 1 : 0;
-  const currentChar = state.words[currentIndex];
-  const currentElm = document.getElementById(`${currentChar}-${currentIndex}`);
-  return {currentElm, currentIndex};  
+function getCurrentElmAndIndex() {
+  const currentIndex = state.input.length ? state.input.length - 1 : 0
+  const currentChar = state.words[currentIndex]
+  const currentElm = document.getElementById(`${currentChar}-${currentIndex}`)
+  return { currentElm, currentIndex }
 }
 
 function handleBackspacePress() {
-  const { currentElm } = getCurrentElmAndIndex();    
-  ["valid", "invalid"].forEach(clsName => currentElm.classList.remove(clsName));  
-  state.input = state.input.slice(0,-1)
+  const { currentElm } = getCurrentElmAndIndex()
+  ;['valid', 'invalid'].forEach(clsName => currentElm.classList.remove(clsName))
+  state.input = state.input.slice(0, -1)
 }
 
-function handleKeyUp(e) {  
-  if(!(allowedKeys.includes(e.code) || ['Key', "Digit", "Numpad"].includes(e.code.slice(0,-1)))){
-    return;
-  } 
+function handleKeyUp(e) {
+  if (
+    !(
+      allowedKeys.includes(e.code) ||
+      ['Key', 'Digit', 'Numpad'].includes(e.code.slice(0, -1))
+    )
+  ) {
+    return
+  }
   input.value = ''
-  state.input += e.key;  
+  state.input += e.key
 }
 
 function resetState() {
-  state.input = "";
-  state.speed = 0;
-  startTime = null;
-  input.value = "";
-  preview.innerHTML = "";
-  setupWords(randomWords(30));
-  renderWords();
+  state.input = ''
+  state.speed = 0
+  startTime = null
+  input.value = ''
+  preview.innerHTML = ''
+  setupWords(randomWords(30))
+  renderWords()
 }
 
 function setupWords(words) {
-  state.words = words.join(" ").split("").concat(" ");
+  state.words = words.join(' ').split('').concat(' ')
 }
 
 function renderSpeed() {
   effect(() => {
-    speed.innerHTML = state.speed + " WPM";
-  });
-}
-
-function checkAndMarkLetter() {
-  effect(() =>{    
-    if(!state.input)  {
-      renderWords();
-      return
-    }   
-
-    const { currentElm, currentIndex } = getCurrentElmAndIndex();
-    const isCorrectChar = state.words[currentIndex] === state.input[currentIndex]
-    currentElm.classList.add(isCorrectChar ? "valid": "invalid");
-
-    if(isCorrectChar){
-      state.speed = calcSpeed(startTime, state.input.length);
-    }
-    
-    // cursor movement
-    const box = currentElm.getBoundingClientRect();
-    const width = 3;
-    const caretHeight = box.height / 1.4;
-    Object.assign(caret.style, {
-      top: box.y + (box.height - caretHeight * 1.22) + "px",
-      height: caretHeight + "px",
-      width: width + "px",
-      bottom: box.height + caretHeight + "px",
-    });
-    animate({
-      type: "keyframes",
-      ease: linear,
-      duration: 125,
-      from: caret.style.left,
-      to: box.left - width + box.width + "px",
-      onUpdate: (latest) => (caret.style.left = latest),
-    }); 
+    speed.innerHTML = state.speed + ' WPM'
   })
 }
 
-function renderWords() { 
+function checkAndMarkLetter() {
+  effect(() => {
+    if (!state.input) {
+      renderWords()
+      return
+    }
+
+    const { currentElm, currentIndex } = getCurrentElmAndIndex()
+    const isCorrectChar =
+      state.words[currentIndex] === state.input[currentIndex]
+    currentElm.classList.add(isCorrectChar ? 'valid' : 'invalid')
+
+    if (isCorrectChar) {
+      state.speed = calcSpeed(startTime, state.input.length)
+    }
+
+    // cursor movement
+    const box = currentElm.getBoundingClientRect()
+    const width = 3
+    const caretHeight = box.height / 1.4
+    Object.assign(caret.style, {
+      top: box.y + (box.height - caretHeight * 1.22) + 'px',
+      height: caretHeight + 'px',
+      width: width + 'px',
+      bottom: box.height + caretHeight + 'px',
+    })
+    animate({
+      type: 'keyframes',
+      ease: linear,
+      duration: 125,
+      from: caret.style.left,
+      to: box.left - width + box.width + 'px',
+      onUpdate: latest => (caret.style.left = latest),
+    })
+  })
+}
+
+function renderWords() {
   if (state.input.length == 0) {
     if (!startTime) {
-      startTime = Date.now();
+      startTime = Date.now()
     }
-  }   
+  }
   state.words.forEach((x, index) => {
-    let exists = true;
-    let elm = document.getElementById(`${x}-${index}`);
+    let exists = true
+    let elm = document.getElementById(`${x}-${index}`)
     if (!elm) {
-      exists = false;
-      elm = document.createElement("span");
-      elm.classList.add("typer-letter");
-      elm.id = `${x}-${index}`;
+      exists = false
+      elm = document.createElement('span')
+      elm.classList.add('typer-letter')
+      elm.id = `${x}-${index}`
     }
-    elm.textContent = x;
+    elm.textContent = x
 
     if (!exists) {
-      preview.append(elm);
+      preview.append(elm)
     }
-  });
-    
-  const { currentElm } = getCurrentElmAndIndex();
+  })
 
-  if (!currentElm) return;
+  const { currentElm } = getCurrentElmAndIndex()
 
-  const box = currentElm.getBoundingClientRect();
-  const width = 3;
-  const caretHeight = box.height / 1.4;
+  if (!currentElm) return
+
+  const box = currentElm.getBoundingClientRect()
+  const width = 3
+  const caretHeight = box.height / 1.4
   Object.assign(caret.style, {
-    top: box.y + (box.height - caretHeight * 1.22) + "px",
-    height: caretHeight + "px",
-    width: width + "px",
-    bottom: box.height + caretHeight + "px",
-  });
+    top: box.y + (box.height - caretHeight * 1.22) + 'px',
+    height: caretHeight + 'px',
+    width: width + 'px',
+    bottom: box.height + caretHeight + 'px',
+  })
   animate({
-    type: "keyframes",
+    type: 'keyframes',
     ease: linear,
     duration: 125,
     from: caret.style.left,
-    to: box.x - width + "px",
-    onUpdate: (latest) => (caret.style.left = latest),
-  }); 
+    to: box.x - width + 'px',
+    onUpdate: latest => (caret.style.left = latest),
+  })
 }
 
 function autoType() {
   effect(() => {
-    let counter = 0;
-    const statement = state.words.join("");
+    let counter = 0
+    const statement = state.words.join('')
     if (autoTypeListener) {
-      clearInterval(autoTypeListener);
+      clearInterval(autoTypeListener)
     }
     autoTypeListener = setInterval(() => {
-      const char = statement[counter++];
-      const evt = new KeyboardEvent("keyup", { key: char, code: keyboardKeyCodeMap[char] })
-      input.dispatchEvent(evt);    
-    }, 30);
-  });
+      const char = statement[counter++]
+      const evt = new KeyboardEvent('keyup', {
+        key: char,
+        code: keyboardKeyCodeMap[char],
+      })
+      input.dispatchEvent(evt)
+    }, 30)
+  })
 }
 
 function calcSpeed(startTime, typedCharacters) {
-  const minutes = 60 * 1000;
-  const totalTimeInMills = Date.now() - startTime;
-  const totalTimeInMinutes = totalTimeInMills / minutes;
-  const typedWords = typedCharacters / 5;
-  return Math.floor(typedWords / totalTimeInMinutes);
+  const minutes = 60 * 1000
+  const totalTimeInMills = Date.now() - startTime
+  const totalTimeInMinutes = totalTimeInMills / minutes
+  const typedWords = typedCharacters / 5
+  return Math.floor(typedWords / totalTimeInMinutes)
 }
 
-(function main() {
-  resetState();
-  checkAndMarkLetter();
-  renderSpeed();
-  input.focus();  
+;(function main() {
+  resetState()
+  checkAndMarkLetter()
+  renderSpeed()
+  input.focus()
   // autoType();
-})();
+})()
